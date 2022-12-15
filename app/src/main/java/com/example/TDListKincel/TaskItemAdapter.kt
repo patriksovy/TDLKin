a98aad3d3eec4bc119370883ec66d17eec64e6ef
patriksovy <patrikincel@gmail.com>
2022-12-15 21:39:47 +0100

    ta fajne

---
+++ app/src/main/java/com/example/TDListKincel/TaskItemAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.TDListKincel
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.TDListKincel.databinding.TaskItemCellBinding
+
+
+class TaskItemAdapter(
+    private val taskItems: List<TaskItem>,
+    private val clickListener: TaskItemClickListener
+): RecyclerView.Adapter<TaskItemViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
+        val from = LayoutInflater.from(parent.context)
+        val binding = TaskItemCellBinding.inflate(from, parent, false)
+        return TaskItemViewHolder(parent.context, binding, clickListener)
+    }
+
+    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        holder.bindTaskItem(taskItems[position])
+    }
+
+    override fun getItemCount(): Int = taskItems.size
+}
